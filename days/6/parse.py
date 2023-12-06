c1715f0db5f6c0db8fe0be8d9bead382e5395d74
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-06 19:14:40 +0100

    Add parse for part 2

--- days/6/parse.py
+++ days/6/parse.py
@@ -22,3 +22,22 @@ def parse_data(example=False): #messy, but works hehe
                     records[i] = int(records[i])
                 break
     return times, records
+
+
+def parse_data_2(example=False): #messy, but works hehe
+    file_name = "data.txt"
+    if example:
+        file_name = "example_data.txt"
+    time = 0
+    record = 0
+    with open(f"days/6/{file_name}", 'r') as file:
+        first_line = True
+        for line in file:
+            line = line.rstrip('\n')
+            line = re.sub(' +', '', line)
+            if(first_line):
+                first_line = False
+                time = int(line[5:])
+            else:
+                record = int(line[9:])
+    return time, record
