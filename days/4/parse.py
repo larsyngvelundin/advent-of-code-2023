8c138cf46dfd092f90f430bd77bd88647738dcbd
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-06 17:15:11 +0100

    Update parse.py

--- days/4/parse.py
+++ days/4/parse.py
@@ -11,3 +11,17 @@ def parse_data(example=False): #messy, but works hehe
             numbers = line[line.find("|")+2:].strip().replace("  ", " ").split(" ")
             data[card_number] = {'numbers': numbers, 'winning_numbers': winning_numbers}
     return data
+
+def parse_data_list(example=False): #messy, but works hehe
+    file_name = "data.txt"
+    if example:
+        file_name = "example_data.txt"
+    data = []
+    with open(f"days/4/{file_name}", 'r') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            card_number = line[line.find("Card")+5:line.find(":")]
+            winning_numbers = line[line.find(":")+2:line.find("|")-1].strip().replace("  ", " ").split(" ")
+            numbers = line[line.find("|")+2:].strip().replace("  ", " ").split(" ")
+            data.append({'numbers': numbers, 'winning_numbers': winning_numbers})
+    return data
