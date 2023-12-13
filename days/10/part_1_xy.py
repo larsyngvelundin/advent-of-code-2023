4f2db8f02d6683967599cb0380f6372273663eda
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-13 22:36:33 +0100

    Day 10 Part 1 alternative

--- days/10/part_1_xy.py
+++ days/10/part_1_xy.py
@@ -1,10 +1,8 @@
 from parse import parse_data
 
-data = parse_data(example=1)
-print(data)
+data = parse_data(example=0)
 
 up, right, down, left = 0, 1, 2, 3
-print(up, right, down, left)
 directions = {
     '-': [False, True, False, True],
     '|': [True, False, True, False],
@@ -25,81 +23,56 @@ def find_start(data):
     return [None, None]
 
 def find_first_step(start_position):
-    # #up
+    #up
     sp = start_position
     if(sp[1] > 0):
         tile_up = data[sp[1]-1][sp[0]]
-        print(tile_up)
         if (d[tile_up][down]):
-            print("found start")
             return [sp[0],sp[1]], [sp[0],sp[1]-1]
     #down
     if(sp[1] < len(data)):
         tile_down = data[sp[1]+1][sp[0]]
-        print(tile_down)
         if (d[tile_down][up]):
-            print("found start")
             return [sp[0],sp[1]], [sp[0],sp[1]+1]
     #left
     if(sp[0] > 0):
         tile_left = data[sp[1]][sp[0]-1]
-        print(tile_left)
         if (d[tile_left][right]):
-            print("found start")
             return [sp[0],sp[1]], [sp[0]-1,sp[1]]
     #right
     if(sp[0] < len(data[0])):
         tile_right = data[sp[1]][sp[0]+1]
-        print(tile_right)
         if (d[tile_right][left]):
-            print("found start")
             return [sp[0],sp[1]],[sp[0]+1,sp[1]]
     
     return [sp[0],sp[1]], [0,0]
-    pass
 
 def find_next(current, next):
-    print("==================================")
-    print("New find_next")
     n = next
     if(data[n[1]][n[0]] == "S"):
         return False, False
-    
-    # print(f"[n[0]+1,n[1]]: {[n[0]+1,n[1]]}")
-    print(f"current {current}")
-    print(f"next {next}")
-    # print(f"[n[0],n[1]-1] {[n[0],n[1]-1]}")
+    ct = data[next[1]][next[0]]
     #up
     if(n[1] > 0 and [n[0],n[1]-1] != current):
         tile_up = data[n[1]-1][n[0]]
-        print(tile_up)
-        if (d[tile_up][down]):
-            print(f"found next up: {tile_up}")
+        if (d[tile_up][down] and d[ct][up]):
             return [n[0],n[1]], [n[0],n[1]-1]
     #down
     if(n[1] < len(data)-1 and [n[0],n[1]+1] != current):
         tile_down = data[n[1]+1][n[0]]
-        print(tile_down)
-        if (d[tile_down][up]):
-            print(f"found next tile_down: {tile_down}")
+        if (d[tile_down][up] and d[ct][down]):
             return [n[0],n[1]], [n[0],n[1]+1]
     #left
     if(n[0] > 0 and [n[0]-1,n[1]] != current):
         tile_left = data[n[1]][n[0]-1]
-        print(tile_left)
-        if (d[tile_left][right]):
-            print(f"found next tile_left: {tile_left}")
+        if (d[tile_left][right] and d[ct][left]):
             return [n[0],n[1]], [n[0]-1,n[1]]
     #right
     if(n[0] < len(data[0])-1 and [n[0]+1,n[1]] != current):
         tile_right = data[n[1]][n[0]+1]
-        print(tile_right)
-        if (d[tile_right][left]):
-            print(f"found next tile_right: {tile_right}")
+        if (d[tile_right][left] and d[ct][right]):
             return [n[0],n[1]],[n[0]+1,n[1]]
-    
-    # return [sp[0],sp[1]], [0,0]
-    # return [0,0], [0,0]
+
 
 def print_map(data):
     for row in data:
@@ -109,20 +82,17 @@ def print_map(data):
                 replace("7", "╗").
                 replace("J", "╝").
                 replace("L", "╚"))
-        
-def main(data):
+
+
+def main():
     start_position = find_start(data)
-    print(f"Start is at {start_position}")
-    print_map(data)
     current, next = find_first_step(start_position)
-    print(f"Starting at {current}, going to {next}")
     steps = 0
     while next:
         current, next = find_next(current, next)
         steps += 1
-        print(f"{steps} steps taken")
     print(f"Total steps: {steps}")
     print(f"Furthest should be {steps/2}")
 
 if __name__ == "__main__":
-    main(data)
+    main()
